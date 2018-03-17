f45315f2247c1559ff72ee84ff7c2ea8de771914
Kim Jooeon <joo2theeon@gmail.com>
2018-03-18 03:24:09 +0900

    [member.models] member init

--- orange/member/models.py
+++ orange/member/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    LEADER_CHOICE = ((1, 'leader'), (0, 'not_leader'))
+    CLOUDER_CHOICE = ((1, 'clouder'), (0, 'not_clouder'))
+    SEX_CHOICE = ((1, 'male'), (0, 'female'))
+
+    name = models.CharField(max_length=5, blank=False)
+    leader = models.IntegerField(choices=LEADER_CHOICE)
+    clouder = models.IntegerField(choices=CLOUDER_CHOICE)
+    sex = models.IntegerField(choices=SEX_CHOICE)
+    activity = models.IntegerField()
+    age = models.IntegerField()
+    closeness = models.ManyToManyField(
+        'Member', symmetrical=False, through='Closeness',
+        )
+
+
+class Closeness(models.Model):
+    pass
+
+
+
